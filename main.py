2c9388da15de155481ccbf832d013daef5d9d952
pocketcat3 <114851989+pocketcat3@users.noreply.github.com>
2022-12-28 15:40:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from settings import *
 from player import Player
 from map import world_map
 from ray_casting import ray_casting
+from drawing import Drawing
 import math
 
 pygame.init()
 sc = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 player = Player()
+drawing = Drawing(sc)
 
 while True:
     for event in pygame.event.get():
@@ -17,16 +19,9 @@ while True:
     player.movement()
     sc.fill(BLACK)
 
-    pygame.draw.rect(sc, DARKGRAY, (0, 0, WIDTH, HALF_HEIGHT))
-    pygame.draw.rect(sc, DARKGRAY, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
-
-    ray_casting(sc, player.pos, player.angle)
-
-    # pygame.draw.circle(sc, GREEN, (int(player.x), int(player.y)), 12)
-    # pygame.draw.line(sc, GREEN, player.pos, (player.x  + WIDTH * math.cos(player.angle),
-    #                                          player.y + WIDTH * math.sin(player.angle)))
-    # for x, y in world_map:
-    #     pygame.draw.rect(sc, DARKGRAY, (x, y, TILE, TILE), 2)a
+    drawing.background()
+    drawing.world(player.pos, player.angle)
+    drawing.fps(clock)
 
     pygame.display.flip()
-    clock.tick(FPS)
+    clock.tick(60)
